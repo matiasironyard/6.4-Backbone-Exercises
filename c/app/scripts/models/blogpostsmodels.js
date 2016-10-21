3b15646dc9aec7919b542bd40c891771da95bffd
Matias Mariani <matiasironyard@gmail.com>
2016-10-21 15:13:55 -0400

    html and models setup

---
+++ c/app/scripts/models/blogpostsmodels.js
@@ -0,0 +1,24 @@
+var Backbone = require('backbone');
+
+// *********************************model setup***********************************************
+
+var BlogPostModel = Backbone.Model.extend({
+  idAttribute: '_id',
+  defaults: {
+    visible: true
+  }
+});
+
+// *********************************Collection setup***********************************************
+
+var BlogPostsCollection = Backbone.Collection.extend({
+  model: BlogPostModel,
+  url: "https://tiny-lasagna-server.herokuapp.com/collections/mmblog",
+});
+
+// *********************************Exports Setup**********************************************
+
+module.exports = {
+  BlogPostModel: BlogPostMode,
+  BlogPostsCollection: BlogPostsCollection,
+};
