d2b2fd5c3b4216d18b7df884071a94d913a0bf61
Matias Mariani <matiasironyard@gmail.com>
2016-10-22 17:54:00 -0400

    router setup, working on templates

--- d/app/scripts/router.js
+++ d/app/scripts/router.js
@@ -1,6 +1,7 @@
 var Backbone = require('backbone');
 var models = require('./models/urltagmodels.js');
 var views = require('./views/urltagviews.js');
+var $ = require('jquery');
 
 // 5 -------------------------------- router set up -------------------------------------------------------
 
@@ -14,11 +15,44 @@ var AppRouter = Backbone.Router.extend({
   initialize: function(){
     this.collection = new models.UrlTagsCollections();
     this.collection.fetch();
+    console.log('models.UrlTagsCollection', this.collection);
   },
+
+  // 6 ----------------------------- index setup ------------------------------------------------------
 
   index: function(){
     var addUrlForm = new views.AddUrlForm({collection: this.collection});
     var urlListing = new views.UrlListing({collection: this.collection});
+    // console.log(urlListing);
     // var tagListing = new views.TagListing({collection: this.collection});
+
+    $('.url-form')
+    .html(addUrlForm.render().el);
+
+    $('.url-listings')
+    .append(urlListing.render().el);
   },
+
+  // 7 --------------------------- bookmarks setup -------------------------------------------------
+
+  getBookmarks: function(id){
+    var self = this;
+    var bookmarks = this.collection.get(id);
+
+    if(!bookmark){
+      this.collection.fetch().then(function(){
+        self.getBookmarks(id);
+      });
+      return;
+    }
+
+    var tagUrlListing = new views.TagUrlListing({model: UrlTagModel});
+
+    $('.tag-listing').html(tagUrlListing.render().el);
+  },
+
 });
+
+// 8 --------------------------------router setup and exports ------------------------------------
+var router = new AppRouter();
+module.exports = router;
