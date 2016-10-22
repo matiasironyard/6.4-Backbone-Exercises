2ba0e20b75792ddfca70b480e7146d83034f91ce
Matias Mariani <matiasironyard@gmail.com>
2016-10-22 17:12:54 -0400

    index.js setup

--- d/app/scripts/index.js
+++ d/app/scripts/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+var Backbone = require('backbone');
+var $ = require('jquery');
+require('./router');
+
+// 4 ----------------------------history set up -----------------------------
+
+//DOM Ready
+
+$(function(){
+  Backbone.history.start();
+});
